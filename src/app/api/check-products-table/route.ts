83386c8a61170d6dce27754788fea44a662a308c
senaprasena168 <senaprasena168@gmail.com>
2025-08-01 08:35:55 +0700

    preparation for initial deployment

--- src/app/api/check-products-table/route.ts
+++ src/app/api/check-products-table/route.ts
@@ -1,4 +1,4 @@
-r.import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
